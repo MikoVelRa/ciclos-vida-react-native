c8eca059ab37243e3169c2f0556d0a6b5da7b8aa
Miguel Arturo <mike@my-link.com>
2020-08-26 18:06:55 -0500

    Ejemplo inicializado

--- components/Navigator/index.js
+++ components/Navigator/index.js
@@ -2,16 +2,37 @@ import React, {Component} from 'react';
 import {View, Text, Stylesheet} from 'react-native';
 import {Navigation} from 'react-native-navigation';
 
-class ReactNav extends Component {
-  constructor(...props) {
-    super(...props);
-  }
+const HomeScreen = (props) => {
+  return (
+    <View style={styles.root}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
 
-  render() {
-    return (
-      <View>
-        <Text>Por hacer!!!</Text>
-      </View>
-    );
-  }
-}
+Navigation.registerComponent('Home', () => HomeScreen); //componentes dentro de la navegación con nombre único.
+
+Navigation.events().registerAppLaunchedListener(async () => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'Home',
+            },
+          },
+        ],
+      },
+    },
+  });
+});
+
+const styles = Stylesheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'whitesmoke',
+  },
+});
