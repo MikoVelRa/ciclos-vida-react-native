8f968f81fa8a303d52d0886af5a9132a07033993
Miguel Arturo <mike@my-link.com>
2020-08-26 17:55:13 -0500

    Ejemplo inicializado

---
+++ components/Navigator/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {View, Text, Stylesheet} from 'react-native';
+import {Navigation} from 'react-native-navigation';
+
+class ReactNav extends Component {
+  constructor(...props) {
+    super(...props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Por hacer!!!</Text>
+      </View>
+    );
+  }
+}
